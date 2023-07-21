3ba270531f182af482514cc31e8ec35e8bc4cce8
retr080s <retr080s@protonmail.com>
2023-07-22 00:27:20 +0200

    added archived web pages section, dark web ...

--- rabbit.py
+++ rabbit.py
@@ -1,5 +1,3 @@
-import webbrowser
-
 print("""\
 
 Wake up...
@@ -12,29 +10,47 @@ https://github.com/retr080s
 
 while True:
   
-  print("1) Maps/Address search")
-  print("2) Username Search")
-  print("3) Name Search")
-  print("4) Quit")
+  print("1) - Archived Web Pages")
+  print("2) - Dark Web")
+  print("3) - Documents")
+  print("4) - Crypto Wallet Tracker")
+  print("5) - Plane/Ship Tracker")
+  print("99) - Exit")
   
   choice = input("Enter Choice: ")
   
   #choice = choice.strip()
    
   if (choice == "1"):
-    print("Enter the address: ")
-    address = input()
-    urlone = "https://www.google.com/maps/place/" + address
-    urltwo = "https://duckduckgo.com/?va=v&t=ha&q=" + address + "&ia=web&iaxm=about&iax=images"
-    urlthree = "https://www.openstreetmap.org/search?query=" + address
-    webbrowser.open(urlone)
-    webbrowser.open(urltwo)
-    webbrowser.open(urlthree)
+    print("Here are some links that might help you: ")
+    print("[+] https://archive.is")
+    print("[+] https://archive.org")
+    print("[+] https://github.com/hartator/wayback-machine-downloader")
+    print("[+] https://www.aware-online.com/osint-tools/web-archive-search-tool/")
+    print("[+] https://cachedview.com/")
+    print("[+] https://www.cachedpages.com/")
   elif (choice == "2"):
-    print("2")
+    print("Dark web search engines: ")
+    print("[+] https://tor.taxi/")
+    print("[+] https://ahmia.fi/")
+    print("[+] https://darkwebnews.com/")
+    print("[+] https://www.deepweb-sites.com/deep-web-search-engines/")
+    print("[+] https://www.torscan.io/")
+    print("[+] https://onions.es/")
   elif (choice == "3"):
-    print("3")
+    print("Search engine used for finding documents: ")
+    print("[+] http://www.findpdfdoc.com/")
   elif (choice == "4"):
+    print("Finding information about cryptocurrencies and crypto wallets: ")
+    print("[+] https://addresswatcher.com/")
+    print("[+] https://www.walletexplorer.com/")
+    print("[+] https://oxt.me/")
+    print("[+] https://www.fsma.be/en/warnings/companies-operating-unlawfully-in-belgium?field_type_of_fraude_tid_i18n=10595&submit=Apply")
+    print("[+] http://bitcoinwhoswho.com/")
+    print("[+] https://blockchain.info/")
+    print("[+] http://bitcoinwhoswho.com/")
+    print("[+] https://www.aware-online.com/en/osint-tools/cryptocurrency-tools/")
+  elif (choice == "99"):
     break
   else:
     print("Invalid Option. Please Try Again.")
